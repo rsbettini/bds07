27ae6c9c069145ca001d2190f5a660195d8f547c
rsbettini <robson.bettini@hotmail.com>
2022-03-31 21:15:35 -0300

    Navbar stylling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,9 +1,13 @@
 import "./assets/styles/custom.scss";
 import "./App.css";
+import Navbar from "components/Navbar";
 
 function App() {
   return (
-    <h1> Shop Car</h1>
+    <>
+    <Navbar />
+    <h1> Carros Top</h1>
+    </>
   );
 }
 
